5d741b5159b81c9d723c161c15bee88ddbc229d0
predrag.skokovic <pskokovic@qualityhouse.com>
2023-01-29 13:40:49 +0100

    removed solution, small fix

--- src/test/java/com/qualityhouse/course/ait/a_linear_scripting/login/LoginImprovedTests.java
+++ src/test/java/com/qualityhouse/course/ait/a_linear_scripting/login/LoginImprovedTests.java
@@ -78,52 +78,11 @@ public class LoginImprovedTests {
 
     @Test
     public void tcLoginWithSeveralUsers() {
-        // todo: Exercise no. 1.1 - perform login (and logout) of three users (student1, student2, student3), sequentially
+        // todo: Exercise no. 1.2 - perform login (and logout) of three users (student1, student2, student3), sequentially
         // note: apply Before and After methods
 
         // Solution:
 
-
-        // login as the first user
-        driver.findElement(By.cssSelector("a[href='index.php?page=login']")).click();
-
-        driver.findElement(By.id("username")).sendKeys("student1");
-
-        driver.findElement(By.name("userpass")).sendKeys("stpass1");
-
-        driver.findElement(By.name("ses_login")).click();
-
-        driver.findElement(By.cssSelector("a[href='index.php?page=logout']")).click();
-
-        driver.findElement(By.cssSelector("a.big-btn[href='index.php?ses_logout=1']")).click();
-
-        // login as the second user
-        driver.findElement(By.cssSelector("a[href='index.php?page=login']")).click();
-
-        driver.findElement(By.id("username")).sendKeys("student2");
-
-        driver.findElement(By.name("userpass")).sendKeys("stpass2");
-
-        driver.findElement(By.name("ses_login")).click();
-
-        driver.findElement(By.cssSelector("a[href='index.php?page=logout']")).click();
-
-        driver.findElement(By.cssSelector("a.big-btn[href='index.php?ses_logout=1']")).click();
-
-        // login as the third user
-        driver.findElement(By.cssSelector("a[href='index.php?page=login']")).click();
-
-        driver.findElement(By.id("username")).sendKeys("student3");
-
-        driver.findElement(By.name("userpass")).sendKeys("stpass3");
-
-        driver.findElement(By.name("ses_login")).click();
-
-        driver.findElement(By.cssSelector("a[href='index.php?page=logout']")).click();
-
-        driver.findElement(By.cssSelector("a.big-btn[href='index.php?ses_logout=1']")).click();
-
-        Assert.assertTrue(driver.findElement(By.cssSelector("a[href='index.php?page=login']")).isDisplayed(), "User is not logged out.");
     }
 
 }
--- src/test/java/com/qualityhouse/course/ait/a_linear_scripting/profile/EditProfileTests.java
+++ src/test/java/com/qualityhouse/course/ait/a_linear_scripting/profile/EditProfileTests.java
@@ -54,101 +54,10 @@ public class EditProfileTests {
 
     @Test
     public void tcVerifyMandatoryFields() {
-        // todo: Exercise no. 2 - Verify that mandatory fields (E-mail, Name, Phone,Address) do not accept empty values
+        // todo: Exercise no. 2.1 - Verify that mandatory fields (E-mail, Name, Phone,Address) do not accept empty values
         // hint: driver.switchTo().alert().dismiss();
 
         // Solution:
 
-
-        WebDriver driver = new ChromeDriver();
-
-        driver.get("http://acme.qualityhouse.com/build3/index.php");
-
-        driver.findElement(By.cssSelector("a[href='index.php?page=login']")).click();
-
-        // login with Student1 credentials
-        driver.findElement(By.id("username")).sendKeys("student1");
-
-        driver.findElement(By.name("userpass")).sendKeys("stpass1");
-
-        driver.findElement(By.name("ses_login")).click();
-
-        // navigate to Profile page
-        driver.findElement(By.cssSelector("a[href='index.php?page=edit-profile']")).click();
-
-        // verify if E-mail is mandatory
-        driver.findElement(By.name("cEmail")).clear();
-
-        driver.findElement(By.name("cName")).clear();
-        driver.findElement(By.name("cName")).sendKeys("Student One");
-
-        driver.findElement(By.name("cPhone")).clear();
-        driver.findElement(By.name("cPhone")).sendKeys("+987654321");
-
-        driver.findElement(By.name("cAddress")).clear();
-        driver.findElement(By.name("cAddress")).sendKeys("Students Street 1\nStudents City");
-
-        driver.findElement(By.cssSelector("input[type='submit'][value='Submit']")).click();
-
-        Alert alert = driver.switchTo().alert();
-        Assert.assertTrue(alert.getText().contains("Please fill all required fields!"));
-        alert.dismiss();
-
-        // verify if Name is mandatory
-        driver.findElement(By.name("cEmail")).clear();
-        driver.findElement(By.name("cEmail")).sendKeys("student1@example.com");
-
-        driver.findElement(By.name("cName")).clear();
-
-        driver.findElement(By.name("cPhone")).clear();
-        driver.findElement(By.name("cPhone")).sendKeys("+987654321");
-
-        driver.findElement(By.name("cAddress")).clear();
-        driver.findElement(By.name("cAddress")).sendKeys("Students Street 1\nStudents City");
-
-        driver.findElement(By.cssSelector("input[type='submit'][value='Submit']")).click();
-
-        alert = driver.switchTo().alert();
-        Assert.assertTrue(alert.getText().contains("Please fill all required fields!"));
-        alert.dismiss();
-
-        // verify if Phone is mandatory
-        driver.findElement(By.name("cEmail")).clear();
-        driver.findElement(By.name("cEmail")).sendKeys("student1@example.com");
-
-        driver.findElement(By.name("cName")).clear();
-        driver.findElement(By.name("cName")).sendKeys("Student One");
-
-        driver.findElement(By.name("cPhone")).clear();
-
-        driver.findElement(By.name("cAddress")).clear();
-        driver.findElement(By.name("cAddress")).sendKeys("Students Street 1\nStudents City");
-
-        driver.findElement(By.cssSelector("input[type='submit'][value='Submit']")).click();
-
-        alert = driver.switchTo().alert();
-        Assert.assertTrue(alert.getText().contains("Please fill all required fields!"));
-        alert.dismiss();
-
-        // verify if Address is mandatory
-        driver.findElement(By.name("cEmail")).clear();
-        driver.findElement(By.name("cEmail")).sendKeys("student1@example.com");
-
-        driver.findElement(By.name("cName")).clear();
-        driver.findElement(By.name("cName")).sendKeys("Student One");
-
-        driver.findElement(By.name("cPhone")).clear();
-        driver.findElement(By.name("cPhone")).sendKeys("+987654321");
-
-        driver.findElement(By.name("cAddress")).clear();
-
-        driver.findElement(By.cssSelector("input[type='submit'][value='Submit']")).click();
-
-        alert = driver.switchTo().alert();
-        // NOTE: this assertion fails, as the Address seems not to be mandatory, although it should be - have we found a bug? :)
-        Assert.assertTrue(alert.getText().contains("Please fill all required fields!"));
-        alert.dismiss();
-
-        driver.close();
     }
 }
